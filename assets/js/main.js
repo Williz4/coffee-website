c4fc14573e553b317ec57c050274428f9ea48374
williz4 <wilsonmwangeka@gmail.com>
2022-05-15 17:00:39 -0600

    Header and Nav Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
 /*=============== LOADER ===============*/
 
-
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+	navToggle = document.getElementById('nav-toggle'),
+	navClose = document.getElementById('nav-close');
 
+//Show the Menu if navToggle is present
+if(navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu');
+	});
+}
+
+//Hide Menu if the navClose is present
+if(navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu');
+	})
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+	const navMenu = document.getElementById('nav-menu');
+	navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
