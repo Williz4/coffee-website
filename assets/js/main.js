3155c771fa437ea6a51b565b95c838ae293e70bc
williz4 <wilsonmwangeka@gmail.com>
2022-05-17 07:44:31 -0600

    Home Section Done and Menu Changing on Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,12 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+	const header = document.getElementById('header');
+	if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
 
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
 
