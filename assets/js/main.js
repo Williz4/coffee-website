fcad05367aa2fb6fdeb4a97170d650e561dcf9f4
williz4 <wilsonmwangeka@gmail.com>
2022-05-19 11:21:16 -0600

    Scroll Up and ScrollBar Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,29 @@ function activeProducts() {
 linkProducts.forEach(l => l.addEventListener('click', activeProducts));
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+	if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
 
-
+window.addEventListener('scroll', scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight,
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute('id');
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+		} else {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+		}
+	})
+}
+
+window.addEventListener('scroll', scrollActive);
